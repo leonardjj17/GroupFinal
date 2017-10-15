754f6933b0c8d8a5b14a11e88b5f83d46841532b
Joe Leonard <Joe Leonard>
2017-10-14 23:19:47 -0500

    Add project files.

---
+++ GroupFinal/Classes/Employee.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupFinal
+{
+    public class Employee
+    {
+        //variable declarations
+        private string employeeID;
+        private string employeeFirst;
+        private string employeeLast;
+        private string employeePhone;
+        private string employeeRole;
+        private DateTime employeeHireDate;
+        private string employeeStatus;
+        private int employeeOrders;
+        //empty constructor
+        public Employee()
+        {
+            employeeID = "";
+            employeeFirst = "";
+            employeeLast = "";
+            employeePhone = "";
+            employeeRole = "";
+            employeeHireDate = DateTime.Now;
+            employeeStatus = "";
+            employeeOrders = 0;
+        }
+        //constructor with all the variables
+        public Employee(string employeeID, string employeeFirst, string employeeLast, string employeePhone, string employeeRole, DateTime employeeHireDate, string employeeStatus, int employeeOrders)
+        {
+            this.employeeID = employeeID;
+            this.employeeFirst = employeeFirst;
+            this.employeeLast = employeeLast;
+            this.employeePhone = employeePhone;
+            this.employeeRole = employeeRole;
+            this.employeeHireDate = employeeHireDate;
+            this.employeeStatus = employeeStatus;
+            this.employeeOrders = employeeOrders;
+        }
+        //get and sets for all employee variables
+        public string EmployeeID { get; set; }
+
+        public string EmployeeFirst { get; set; }
+
+        public string EmployeeLast { get; set; }
+        
+        public string EmployeePhone { get; set; }
+
+        public string EmployeeRole { get; set; }
+
+        public DateTime EmployeeHireDate { get; set; }
+
+        public string EmployeeStatus { get; set; }
+
+        public int EmployeeOrders { get; set; }
+    }
+}
---
+++ GroupFinal/Classes/Pizza.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupFinal
+{
+    public class Pizza : Products
+    {
+        //declaring variables
+        private string pizzaToppings, pizzaCheese, pizzaSauce, pizzaCrust, pizzaExtras, pizzaSize;
+        private double pizzaPrice;
+
+        //empty constructor
+        public Pizza()
+        {
+            pizzaToppings = "";
+            pizzaCheese = "";
+            pizzaSauce = "";
+            pizzaCrust = "";
+            pizzaExtras = "";
+            pizzaSize = "";
+            pizzaPrice = 0;
+        }
+
+        //constructor with all the variables
+        public Pizza(string pizzaToppings, string pizzaCheese, string pizzaSauce, string pizzaCrust, string pizzaExtras, string pizzaSize, double pizzaPrice)
+        {
+            this.pizzaToppings = pizzaToppings;
+            this.pizzaCheese = pizzaCheese;
+            this.pizzaSauce = pizzaSauce;
+            this.pizzaCrust = pizzaCrust;
+            this.pizzaExtras = pizzaExtras;
+            this.pizzaSize = pizzaSize;
+            this.pizzaPrice = pizzaPrice;
+        }
+
+        //get and sets for all the variables, these may need to be changed/just the basic ones for right now
+
+
+        public string PizzaToppings { get; set; }
+
+        public string PizzaCheese { get; set; }
+
+        public string PizzaSauce { get; set; }
+
+        public string PizzaCrust { get; set; }
+
+        public string PizzaExtras { get; set; }
+
+        public string PizzaSize { get; set; }
+
+        public double PizzaPrice { get; set; }
+    }
+}
