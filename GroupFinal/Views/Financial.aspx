fda4892d277b8d402b6a2d50f3e64cb0ef18e7a0
Brandon <bjrough1@gmail.com>
2017-10-18 19:22:21 -0500

    Added a financial page, blank for right now

---
+++ GroupFinal/Views/Financial.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Financial.aspx.cs" Inherits="GroupFinal.Views.Financial" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <p>
+        This is where I&#39;ll put financial info.</p>
+</asp:Content>
---
+++ GroupFinal/Views/Financial.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace GroupFinal.Views
+{
+    public partial class Financial : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
