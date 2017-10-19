30cc261c541c17cc9c14df171aca078e543d6d1e
SJB <SJB@DESKTOP-MT2N6F2>
2017-10-18 19:49:16 -0500

    fixed connection string

--- GroupFinal/Database/Connection.cs
+++ GroupFinal/Database/Connection.cs
@@ -12,7 +12,8 @@ namespace GroupFinal.DA
         {
             try
             {
-                SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["database"].ConnectionString);
+                string connectionString = @"Data Source = quentindb.db.2823567.59f.hostedresource.net; Initial Catalog = QuentinDB; User ID = OsheaDB2644; Password = s#blaRNey!01";
+                SqlConnection connection = new SqlConnection(connectionString);
                 return connection;
             }
             catch (Exception ex)
