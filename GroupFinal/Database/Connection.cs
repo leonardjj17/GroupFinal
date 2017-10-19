3c7f083fa7aaf16a74ce144b27c74554ec6769db
mm147988 <mm147988@LR00104>
2017-10-18 19:37:56 -0500

    started DAs, a little work on employees pages, created connection string

---
+++ GroupFinal/Database/Connection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GroupFinal.DA
+{
+    public static class Connection
+    {
+        public static SqlConnection getConnection()
+        {
+            try
+            {
+                SqlConnection connection = new SqlConnection(System.Configuration.ConfigurationManager.ConnectionStrings["database"].ConnectionString);
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ GroupFinal/Database/Employees.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+
+namespace GroupFinal.DA
+{
+    public class Employees
+    {
+        public Employee GetEmployeeByLogin(String Login)
+        {
+            if (Login == null) return null;
+            String query = "SELECT * FROM Employees WHERE Role in ('Employee', 'Manager') AND Login = @Login";
+            SqlCommand cmd = new SqlCommand(query);
+            cmd.Parameters.AddWithValue("Login", Login);
+
+        }
+    }
+}
