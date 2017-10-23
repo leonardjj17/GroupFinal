b051ed5e360be8333e1e4e4d12b2db3e79f37fa8
Brandon <bjrough1@gmail.com>
2017-10-23 18:45:14 -0500

    edit to employee, should be all int employeeID now

--- GroupFinal/Classes/Finacial.cs
+++ GroupFinal/Classes/Finacial.cs
@@ -7,6 +7,19 @@ namespace GroupFinal.Classes
 {
     public class Finacial
     {
-        private string 
+        //the only thing I figure we needed for right now was storeBalance
+        private double storeBalance;
+        private int storeNum;
+
+        //Class to subtract from the total
+        public void DeductBalance(int amountToSubtract)
+        {
+            storeBalance = storeBalance - amountToSubtract;
+        }
+        //Class to add to the total
+        public void AddBalance(int amountToAdd)
+        {
+            storeBalance = storeBalance + amountToAdd;
+        }
     }
 }
