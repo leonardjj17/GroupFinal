e94ace22c049e6990a1ed486afbe283243ccd83b
SJB <SJB@DESKTOP-MT2N6F2>
2017-10-23 19:52:13 -0500

    fixed customer class and DA

---
+++ GroupFinal/Classes/Customer.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GroupFinal
+{
+    public class Customer
+    {
+        private int customerID;
+        private string customerFirst, customerLast, customerAddress, customerCity, customerState, customerPhone, customerRole, primaryStore, customerLogin, customerPassword, customerZIP;
+
+        public Customer()
+        {
+
+        }
+
+        public Customer(int customerID, string customerFirst, string customerLast, string customerAddress, string customerCity, 
+            string customerState, string customerZip, string customerPhone, string customerRole, string primaryStore, string customerLogin, string customerPassword)
+
+        {
+            this.customerID = CustomerID;
+            this.customerFirst = CustomerFirst;
+            this.customerLast = CustomerLast;
+            this.customerAddress = CustomerAddress;
+            this.customerCity = CustomerCity;
+            this.customerState = CustomerState;
+            this.customerZIP = CustomerZip;
+            this.customerPhone = CustomerPhone;
+            this.customerRole = CustomerRole;
+            this.primaryStore = PrimaryStore;
+            this.customerLogin = CustomerLogin;
+            this.customerPassword = CustomerPassword;
+        }
+
+        public int CustomerID { get; set; }
+        public string CustomerFirst { get; set; }
+        public string CustomerLast { get; set; }
+        public string CustomerAddress { get; set; }
+        public string CustomerCity { get; set; }
+        public string CustomerState { get; set; }
+        public string CustomerPhone { get; set; }
+        public string CustomerZip { get; set; }
+        public string CustomerRole { get; set; }
+        public string PrimaryStore { get; set; }
+        public string CustomerLogin { get; set; }
+        public string CustomerPassword { get; set; }
+
+
+
+    }
+}
