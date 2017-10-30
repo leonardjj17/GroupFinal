ac55728ddfdded5918f8df26673c450cfbc1ee34
Marco A <marcoaldana2@gmail.com>
2017-10-30 18:27:36 -0500

    Created Pizza View

---
+++ GroupFinal/Views/PizzaView.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using GroupFinal.Classes;
+
+namespace GroupFinal.Views
+{
+    public partial class PizzaView : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Pizza newPizza = new Pizza();
+        }
+    }
+}
