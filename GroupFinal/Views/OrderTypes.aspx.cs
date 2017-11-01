228adb1f6f3c9fa5c357d60ac0416d733c98a85e
SJB <SJB@DESKTOP-MT2N6F2>
2017-11-01 18:20:52 -0500

    redid order type

---
+++ GroupFinal/Views/OrderTypes.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using GroupFinal.Classes;
+
+namespace GroupFinal.Views
+{
+    public partial class OrderTypes : System.Web.UI.Page
+    {
+        Order OrderType = null;
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        protected void rdoDelivery_CheckedChanged(object sender, EventArgs e)
+        {
+            OrderType = delivery;
+        }
+
+        protected void rdoCarryOut_CheckedChanged(object sender, EventArgs e)
+        {
+            OrderType = "carryout";
+        }
+
+        protected void btnOrderType_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
