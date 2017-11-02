ed5504d25cd77da8df0b918f3e1751776b95e449
SJB <SJB@DESKTOP-MT2N6F2>
2017-11-01 19:32:35 -0500

    added the masterpage to order type

--- GroupFinal/Views/orderType.aspx.designer.cs
+++ GroupFinal/Views/orderType.aspx.designer.cs
@@ -13,15 +13,6 @@ namespace GroupFinal.Views {
     public partial class orderType {
         
         /// <summary>
-        /// form1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
         /// rdoDelivery control.
         /// </summary>
         /// <remarks>
@@ -56,6 +47,24 @@ namespace GroupFinal.Views {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.TextBox txtZip;
+        
+        /// <summary>
+        /// DropDownList1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownList1;
+        
+        /// <summary>
+        /// OsheaDB control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource OsheaDB;
         
         /// <summary>
         /// btnOrderType control.
