07b79d06582840c345cda9cdcc8ad55968bb34c0
Joe <leonardjj09@gmail.com>
2017-11-06 19:45:21 -0600

    updated bootstrap, add images

--- GroupFinal/Classes/Cart.cs
+++ GroupFinal/Classes/Cart.cs
@@ -7,17 +7,17 @@ namespace GroupFinal.Classes
 {
     public class Cart
     {
-        public List<Products> CartItems = new List<Products>();
-        public static double CalcSubtotal(List<Products> CartItems)
-        {
-            double subtotal = 0;
-            foreach (Products p in CartItems)
-            {
-                subtotal += (double)p.ProductMenuPrice;
+        //public List<Products> CartItems = new List<Products>();
+        //public static double CalcSubtotal(List<Products> CartItems)
+        //{
+        //    double subtotal = 0;
+        //    foreach (Products p in CartItems)
+        //    {
+        //        subtotal += (double)p.ProductMenuPrice;
 
-            }
+        //    }
 
-            return subtotal;
-        }
+        //    return subtotal;
+        //}
     }
 }
