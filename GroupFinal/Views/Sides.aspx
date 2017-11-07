b3028678f38768dd7b95c5de7dcaf0d9d9f61647
Marco A <marcoaldana2@gmail.com>
2017-11-06 19:44:58 -0600

    Sides view done, makes calculations, doesn't save to shopping cart

--- GroupFinal/Views/Sides.aspx
+++ GroupFinal/Views/Sides.aspx
@@ -1,3 +1,8 @@
 ﻿<%@ Page Title="Sides" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Sides.aspx.cs" Inherits="GroupFinal.Views.Sides" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+    <br />
+    Select your side:<asp:Panel ID="pnlSides" runat="server" Width="200px">
+    </asp:Panel>
+    <br />
+    <asp:Button ID="btnSubmit" runat="server" OnClick="btnSubmit_Click" Text="I Want These Sides!" />
 </asp:Content>
--- GroupFinal/Views/Sides.aspx.cs
+++ GroupFinal/Views/Sides.aspx.cs
@@ -4,13 +4,53 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using GroupFinal.DA;
+using GroupFinal.Database;
 
 namespace GroupFinal.Views
 {
     public partial class Sides : System.Web.UI.Page
     {
+        List<Products> allSides = ProductsDA.GetAllSides();
+
         protected void Page_Load(object sender, EventArgs e)
         {
+            foreach(Products sides in allSides)
+            {
+                CheckBox newSide = new CheckBox();
+                newSide.Text = sides.ProductDetail;
+
+                pnlSides.Controls.Add(newSide);
+                pnlSides.Controls.Add(new LiteralControl("<br />"));
+
+            }
+        }
+
+        protected void btnSubmit_Click(object sender, EventArgs e)
+        {
+            double sideTotal = 0;
+            string selectedSides = "";
+            
+
+            foreach(Control sides in pnlSides.Controls)
+            {
+                if (sides.GetType().Name == "CheckBox")
+                {
+                    if(((CheckBox)sides).Checked)
+                    {
+                        string currentSide = ((CheckBox)sides).Text;
+                        selectedSides += currentSide + " ";
+                        foreach (Products s in allSides)
+                        {
+                            if(s.ProductDetail == currentSide)
+                            {
+                                sideTotal += s.ProductPrice;
+                            }
+                        }
+                    }
+                }
+            }
+
 
         }
     }
