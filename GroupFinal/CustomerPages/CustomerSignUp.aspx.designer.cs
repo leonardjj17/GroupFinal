16434d62304918a96dc88f923c02e9dee0526d28
Brandon <bjrough1@gmail.com>
2017-11-08 19:45:45 -0600

    Updated signup to use an email

--- GroupFinal/CustomerPages/CustomerSignUp.aspx.designer.cs
+++ GroupFinal/CustomerPages/CustomerSignUp.aspx.designer.cs
@@ -166,6 +166,15 @@ namespace GroupFinal.CustomerPages {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator7;
         
         /// <summary>
+        /// RegularExpressionValidator2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RegularExpressionValidator RegularExpressionValidator2;
+        
+        /// <summary>
         /// txtPassword control.
         /// </summary>
         /// <remarks>
