8507e5c6b501b9587f13e4b1abc6b2f5b00423b2
SJB <SJB@DESKTOP-MT2N6F2>
2017-11-08 19:35:47 -0600

    Added Specials page and products session

--- GroupFinal/Views/Specials.aspx.cs
+++ GroupFinal/Views/Specials.aspx.cs
@@ -4,14 +4,40 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using GroupFinal.Classes;
 
 namespace GroupFinal.Views
 {
     public partial class Specials : System.Web.UI.Page
     {
+        Products p = new Products();
         protected void Page_Load(object sender, EventArgs e)
         {
+            Session["Products"] = p;
+            Response.Redirect("~/views/CartView");
+        }
+
+        protected void rdoFamPk_CheckedChanged(object sender, EventArgs e)
+        {
+            p.ProductPrice = 19.99;
+            p.ProductType = "Special";
+            p.ProductDetail = "Family Pack";
+        }
+
+        protected void rdoIndivPk_CheckedChanged(object sender, EventArgs e)
+        {
+            p.ProductPrice = 9.99;
+            p.ProductType = "Special";
+            p.ProductDetail = "Individual Pack";
+        }
+
+        protected void rdoLGFamPk_CheckedChanged(object sender, EventArgs e)
+        {
+            p.ProductPrice = 29.99;
+            p.ProductType = "Special";
+            p.ProductDetail = "Large Family Pack";
 
         }
+
     }
 }
