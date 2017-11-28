2069ca2efa16177c1f9c4ce745a734b810f5da55
SJB <SJB@DESKTOP-MT2N6F2>
2017-11-27 19:32:20 -0600

    fixed product classes deleted cut features

--- GroupFinal/Classes/Drink.cs
+++ GroupFinal/Classes/Drink.cs
@@ -16,7 +16,7 @@ namespace GroupFinal.Classes
         {
 
         }
-        public Drink(string drinkSize, string drinkType, double drinkPrice, int productID, double productPrice, string productType, int productQty, double productCost, string productDetail) : base(productID, productPrice, productType, productQty, productCost, productDetail)
+        public Drink(string drinkSize, string drinkType, double drinkPrice, int productID, double productPrice, string productType, int productQty, string productDetail) : base(productID, productPrice, productType, productQty, productDetail)
           
         {
             this.drinkSize = drinkSize;
